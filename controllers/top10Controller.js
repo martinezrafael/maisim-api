a3ff36de1cedffbb0a2dcd23c7c3cc6c9c0aced6
Rafael Molina Martinez <rafael.martinez@rmfarma.com.br>
2023-07-10 14:00:47 -0300

    criada uma nova rota para buscar o top 10 de genéricos

--- controllers/top10Controller.js
+++ controllers/top10Controller.js
@@ -41,4 +41,28 @@ router.get('/:brick', async(req, res) => {
 })
 
 
+//Busca os registros de um brick específico, cujo valor de rank está entre 1 e 10 e o setor_nec_aberto é GENERICO
+router.get('/:brick/:setor_nec_aberto', async(req, res) => {
+  try {
+    const { brick, setor_nec_aberto } = req.params;
+    const topBrick = await Top10Model.findAll({
+      attributes: {
+        exclude: ['id']
+      },
+      where: {
+        brick: brick,
+        setor_nec_aberto: setor_nec_aberto,
+        rank: {
+          [Op.between]: [1,10]
+        }
+      }
+    });
+    res.status(200).json(topBrick);
+  } catch (error) {
+    res.status(500).json({message: `Erro de servidor ${error}`})
+  }
+})
+
+
+
 module.exports = router;
