501662ed5c74c0b8e76de13ecc34ea30dc1b4d34
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2023-07-21 10:35:54 -0300

    ajustei as rotas

--- controllers/top10Controller.js
+++ controllers/top10Controller.js
@@ -39,7 +39,7 @@ router.get("/:brick", async (req, res) => {
   }
 });
 
-router.get("/cep/:user_cep", async (req, res) => {
+router.get("/cep/rank/:user_cep", async (req, res) => {
   try {
     const { user_cep } = req.params;
 
@@ -68,6 +68,43 @@ router.get("/cep/:user_cep", async (req, res) => {
         },
         RANK: {
           [Op.between]: [1, 10],
+        },
+      },
+      order: [["SETOR_NEC_ABERTO", "ASC"]],
+    });
+
+    res.status(200).json(topBrick);
+  } catch (error) {
+    res.status(500).json({ message: `Erro de servidor ${error}` });
+  }
+});
+
+router.get("/cep/:user_cep", async (req, res) => {
+  try {
+    const { user_cep } = req.params;
+
+    console.log(user_cep);
+
+    const cepAsInteger = parseInt(user_cep, 10);
+    console.log("chega aqui");
+    console.log(cepAsInteger);
+
+    if (isNaN(cepAsInteger)) {
+      return res.status(400).json({
+        message: "CEP inválido. Certifique-se de que o CEP é um número válido.",
+      });
+    }
+
+    const topBrick = await Top10Model.findAll({
+      attributes: {
+        exclude: ["id"],
+      },
+      where: {
+        CEP_INICIAL: {
+          [Op.lte]: cepAsInteger,
+        },
+        CEP_FINAL: {
+          [Op.gte]: cepAsInteger,
         },
       },
       order: [["SETOR_NEC_ABERTO", "ASC"]],
